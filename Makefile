606533bf5acda761057252eeab5ed8f619bb4c33
VictorGotsenko <victor.gotsenko@gmail.com>
2024-11-27 19:32:44 +0500

    Add modified Makefile and Data-Validator.yml

---
+++ Makefile
@@ -0,0 +1,23 @@
+.DEFAULT_GOAL := build-run
+
+clean:
+	make -C app clean
+
+build:
+	make -C app build
+
+test:
+	make -C app test
+
+report:
+	make -C app report
+
+lint:
+	make -C app lint
+
+update-deps:
+	make -C app update-deps
+
+build-run: build run
+
+.PHONY: build
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,14 @@
+import com.adarshr.gradle.testlogger.theme.ThemeType
+
 plugins {
     id("java")
     id("application")
     checkstyle
     jacoco
     id("com.adarshr.test-logger") version "4.0.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.50.0"
+
 }
 
 group = "hexlet.code"
@@ -37,6 +42,23 @@ jacoco {
 tasks.test {
     useJUnitPlatform()
     finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+    testlogger {
+        theme = ThemeType.MOCHA
+        showStandardStreams = true
+        showExceptions = true
+        showStackTraces = true
+        showSummary = true
+        showSimpleNames = false
+        showPassed = true
+        showSkipped = true
+        showFailed = true
+        showOnlySlow = false
+        showStandardStreams = false
+        showPassedStandardStreams = true
+        showSkippedStandardStreams = true
+        showFailedStandardStreams = true
+        logLevel = LogLevel.LIFECYCLE
+    }
 }
 
 tasks.jacocoTestReport {
