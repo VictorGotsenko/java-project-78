d21971d1c4c29386f53fc4ea2fd2aa8815344fd7
VictorGotsenko <victor.gotsenko@gmail.com>
2024-12-09 16:37:32 +0500

    Refactor: Some module was improve

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,17 +1,31 @@
 package hexlet.code.schemas;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public abstract class BaseSchema<T> {
+    boolean isRequired = false;
+    private Map<String, Predicate<T>> conditions = new LinkedHashMap<>();
+
+    BaseSchema() {
+        setConditions("isValueNull", Objects::nonNull);
+    }
+
     public final void setConditions(String key, Predicate<T> value) {
         this.conditions.put(key, value);
     }
 
-    private Map<String, Predicate<T>> conditions = new HashMap<>();
-
     public final boolean isValid(T value) {
+
+        if (!isRequired) {
+            var checkNull = conditions.get("isValueNull");
+            if (!checkNull.test(value)) {
+                return true;
+            }
+        }
+
         for (String r : conditions.keySet()) {
             if (!conditions.get(r).test(value)) {
                 return false;
